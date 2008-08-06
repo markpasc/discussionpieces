362f99fbeeb986a572e2d129bd49baa209b19cec
mpaschal <mark@mpaschal.local>
2008-08-06 14:16:05 -0700

    Use Google users api Display creator 'nicknames' on objects

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from google.appengine.ext import webapp
 from google.appengine.ext import db
 from google.appengine.ext.webapp import template
 from google.appengine.api.datastore_errors import BadValueError
+from google.appengine.api import users
 import django.utils.simplejson as json
 
 from models import *
@@ -14,6 +15,9 @@ class ModelEncoder(json.JSONEncoder):
         if isinstance(obj, db.Model):
             jobj = dict([(k, getattr(obj, k, None)) for k in obj.properties().keys()])
             jobj['key'] = str(obj.key())
+            return jobj
+        elif isinstance(obj, users.User):
+            jobj = dict(nickname=obj.nickname(), email=obj.email())
             return jobj
         elif isinstance(obj, db.Property):
             return str(obj)
@@ -26,12 +30,23 @@ class Handlr(webapp.RequestHandler):
         if data is None:
             data = {}
         if kwargs:
-            data.update(kwargs)    
+            data.update(kwargs)
+            
+        data.update({
+            'user':       users.get_current_user(),
+            'login_url':  users.create_login_url('/'),
+            'logout_url': users.create_logout_url('/'),
+        })
+        
         self.response.out.write(template.render('templates/' + tmpl + '.html', data))
     def oops404(self, msg=None):
         self.response.set_status(404)
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.out.write(msg if msg else 'Not found')
+    def oops403(self, msg=None):
+        self.response.set_status(403)
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.out.write(msg if msg else 'Forbidden')
 
 class MainHandler(Handlr):
     def get(self):
@@ -43,6 +58,10 @@ class StuffHandler(Handlr):
         stuff_data = stuff.fetch(10)
         self.json(stuff_data)
     def post(self, key=None):
+        user = users.get_current_user()
+        if not user:
+            self.oops403('Login required')
+            return
         if key:
             p = Piece.get(key)
             if not p:
@@ -50,7 +69,7 @@ class StuffHandler(Handlr):
                 return
         else:
             p = Piece()
-            (p.creator,) = Person.all().fetch(1)
+            p.creator = users.get_current_user()
             p.icon = self.request.get('icon')
             p.name = self.request.get('name')
             p.desc = self.request.get('desc')
--- models.py
+++ models.py
@@ -1,11 +1,8 @@
 
 from google.appengine.ext import db
 
-class Person(db.Model):
-    openid = db.LinkProperty(verbose_name="OpenID")
-
 class Piece(db.Model):
-    creator = db.ReferenceProperty(Person)
+    creator = db.UserProperty()
     x       = db.IntegerProperty()
     y       = db.IntegerProperty()
     icon    = db.StringProperty()
